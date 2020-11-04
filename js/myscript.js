c5154c2072c556d53f3e653d079eb8b9447bfed1
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-11-05 00:52:28 +0100

    Struttura1

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+// Creare l’header come da screeshot allegato.
+// Ogni voce di menù avrà un suo dropdown menù che viene fuori al click.
+// Dettagli
+// 1. prendiamo come ref lo screeshot;
+// 2. il dropdown esce fuori al click non all’hover (sennò  a qualcuno giustamente glie verrebbe da fare in css);
+// 3.basta che fate la parte di header/menu (appunto quello che c’è in screeshot);
+// 4.tenete conto che su questo avete più elementi uguali da comandare e ognuno chiaramente rende visibile solo il proprio dropdown;
+// 5.consiglio: degli eventuali casi limte/controlli (es. voglio che sia aperto solo un dropdown alla volta, preoccupatevi alla fine).
+$(document).ready(function () {
+
+$("li.dropitem1").click(function() {
+  $(".dropdown1").toggleClass("dbloc");
+});
+
+$("li.dropitem2").click(function() {
+  $(".dropdown2").toggleClass("dbloc");
+});
+
+$("li.dropitem3").click(function() {
+  $(".dropdown3").toggleClass("dbloc");
+});
+
+});
